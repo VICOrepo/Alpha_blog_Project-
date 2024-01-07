2d443be2ee6c007b775c4600fbdbd5da5b0910e3
VIRAL JAIN <viral.jain@sigmainfo.net>
2024-01-07 14:22:20 +0530

    error msg flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,29 @@
 class ArticlesController < ApplicationController
 
     def show
-        debugger
         @article = Article.find(params[:id])
     end
 
     def index
         @articles = Article.all
     end
+
+    def new
+    @article = Article.new
+    end
+
+    def create
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      @article.save
+      # redirect_to @article
+       if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+       else
+    # Redirect to the new action to display the form with errors
+        redirect_to new_article_path, alert: @article.errors.full_messages.join(', ')
+       end
+
+    end
+
 end
