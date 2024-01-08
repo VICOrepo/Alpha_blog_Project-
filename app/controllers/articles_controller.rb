83efa32a5f0c863af6c35d049656b0ee1234f910
VIRAL JAIN <viral.jain@sigmainfo.net>
2024-01-08 10:44:43 +0530

    DRY - Dont't Repeat Yourself (Refactoring and partials)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
     end
 
     def index
@@ -13,12 +15,12 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
     end
 
 
     def create
-      @article = Article.new(params.require(:article).permit(:title, :description))
+      @article = Article.new(article_params)
       @article.save
       # redirect_to @article
        if @article.save
@@ -31,8 +33,8 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        # @article = Article.find(params[:id])
+        if @article.update(article_params)
         flash[:notice] = "Article was updated successfully."
         redirect_to @article
         else
@@ -41,9 +43,20 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
     end
 
+    private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  
 end
